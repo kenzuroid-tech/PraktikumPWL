735edff1722b10c9bf3987898b89c8982c56cdde
kenzuroid-tech <sitinikmatussholihahdivti@gmail.com>
2026-03-04 09:06:54 +0700

    filament jb 2

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Dom\Text;
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->unique(ignoreRecord: true)
+                    ->maxLength(255),
+                TextInput::make('password')
+                    ->password()
+                    ->minLength(6)
+                    ->required()
+            ]);
+    }
+}
